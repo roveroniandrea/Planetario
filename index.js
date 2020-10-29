d6544c3b929c5ccc8543c0c062fb56d8fe29d1d4
Andrea Roveroni <roveroniandrea@gmail.com>
2020-10-29 11:20:50 +0100

    code formatting and controls

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
+/** Relative assets' folder location */
 const assetsFolder = 'assets';
 
 window.onload = function () {
+    /** If false, the time is paused */
     var play = true;
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
+    /** All the celestail bodies created */
     var celestialBodies = [];
+
+    var scene = new THREE.Scene();
+
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.matrixAutoUpdate = false;
     camera.matrix = new THREE.Matrix4().makeTranslation(0, 15, 12).multiply(new THREE.Matrix4().makeRotationX((-Math.PI / 2) * 0.5));
     scene.add(camera);
 
-    var sun = new CelestialBody({ radius: 2, scene, isEmissive: true, texture: 'sun.jpg', orbitingSpeed: 0 });
+    var sun = new CelestialBody({ name: 'Sun', radius: 2, scene, isEmissive: true, texture: 'sun.jpg', orbitingSpeed: 0 });
 
-    var earth = new CelestialBody({ radius: 0.6, celestialBody: sun, scene, texture: 'earth.jpg', orbitingDistance: 6, orbitingSpeed: 90 });
+    var earth = new CelestialBody({
+        name: 'Earth',
+        radius: 0.6,
+        celestialBody: sun,
+        texture: 'earth.jpg',
+        orbitingDistance: 6,
+        orbitingSpeed: 90,
+    });
 
     var moon = new CelestialBody({
+        name: 'Moon',
         radius: 0.2,
         celestialBody: earth,
         texture: 'moon.jpg',
-        scene,
         orbitingDistance: 1.2,
         orbitingSpeed: 180,
     });
 
     var mars = new CelestialBody({
+        name: 'Mars',
         radius: 0.5,
-        scene,
         celestialBody: sun,
         orbitingDistance: 8,
         orbitingSpeed: 70,
@@ -39,18 +50,19 @@ window.onload = function () {
 
     //Light
     var directionalLight = new THREE.DirectionalLight(0xffffff, 0.8);
-    directionalLight.position.set(0, 2, 1); // Setto la posizione e lo rendo con lunghezza 1
+    directionalLight.position.set(0, 2, 1);
     scene.add(directionalLight);
 
     //Renderer
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight); //Aspect ratio
-    document.body.appendChild(renderer.domElement); //Lo aggiungo al DOM
+    document.body.appendChild(renderer.domElement); //Adding to DOM
 
-    //Funzione di rendering
+    //Rendering function
     var render_scene = function () {
         var now = Date.now();
-        var dt = now - (render_scene.time || now); //Tempo trascorso tra due chiamate del renderer
+        /** Time passed from the last frame */
+        var dt = now - (render_scene.time || now);
 
         // Date object of current frame
         render_scene.time = now;
@@ -64,7 +76,7 @@ window.onload = function () {
                 cel.orbitStep(dt, render_scene.executionTime);
             }
         }
-        requestAnimationFrame(render_scene); // Renderizza in base al tempo di refresh dello schermo senza che usiamo interval
+        requestAnimationFrame(render_scene);
         renderer.render(scene, camera);
     };
 
@@ -74,6 +86,11 @@ window.onload = function () {
         if (e.code == 'Space') {
             play = !play;
         }
+        if (e.code == 'KeyT') {
+            for (let cel of celestialBodies) {
+                cel.toggleOrbitVisible();
+            }
+        }
     });
 };
 
@@ -82,7 +99,7 @@ window.onload = function () {
  * @param radius The radius of the body
  * @param celestialBody If not null specifies the parent celestial body to rotate around
  * @param orbitingSpeed The speed of the orbit in degrees/sec
- * @param scene The scene to render in
+ * @param scene The scene to render in. Note that either celestialBody or scene must be provided
  * @param color The color of the celestial body
  * @param texture The name of the texture of the body (.extension included)
  * @param isEmissive If true, the body will emit light
@@ -93,43 +110,59 @@ var CelestialBody = function ({
     celestialBody = null,
     orbitingSpeed = 0,
     orbitingDistance = 0,
-    scene,
+    scene = null,
     color = '',
     texture = '',
     isEmissive = false,
 }) {
-    // Carico la texture se fornita
-    var loadedTexture = texture ? new THREE.TextureLoader().load(assetsFolder + '/' + texture) : null;
+    this.name = name;
 
-    //Geometry della sfera
-    var geometry = new THREE.SphereGeometry(radius, 20, 20);
+    /** Sets the mesh of the object */
+    var setMesh = () => {
+        // Load texture if passed
+        var loadedTexture = texture ? new THREE.TextureLoader().load(assetsFolder + '/' + texture) : null;
 
-    //Materiale
-    var material = new THREE.MeshLambertMaterial({
-        color,
-        map: loadedTexture,
-        ...(isEmissive && loadedTexture ? { emissive: 'white', emissiveMap: loadedTexture } : {}),
-    });
+        //Sphere geometry
+        var geometry = new THREE.SphereGeometry(radius, 20, 20);
 
-    /**The mesh used by three.js*/
-    this.mesh = new THREE.Mesh(geometry, material);
-    this.mesh.matrixAutoUpdate = false;
+        //Material
+        var material = new THREE.MeshLambertMaterial({
+            color,
+            map: loadedTexture,
+            ...(isEmissive && loadedTexture ? { emissive: 'white', emissiveMap: loadedTexture } : {}),
+        });
 
-    if (celestialBody) {
-        celestialBody.mesh.add(this.mesh);
-    } else {
-        scene.add(this.mesh);
-    }
+        /**The mesh used by three.js*/
+        this.mesh = new THREE.Mesh(geometry, material);
+        this.mesh.matrixAutoUpdate = false;
 
-    if (celestialBody) {
-        var ringSize = 0.1;
-        var ringGeometry = new THREE.RingGeometry(orbitingDistance - ringSize / 2, orbitingDistance + ringSize / 2, 40);
-        var ringMaterial = new THREE.MeshBasicMaterial({ color: color || 'white', side: THREE.DoubleSide });
-        this.orbitRing = new THREE.Mesh(ringGeometry, ringMaterial);
-        this.orbitRing.matrixAutoUpdate = false;
-        this.orbitRing.matrix = new THREE.Matrix4().makeRotationX(Math.PI / 2);
-        celestialBody.mesh.add(this.orbitRing);
-    }
+        // Orbiting around parent celestial body or in scene
+        if (celestialBody) {
+            celestialBody.mesh.add(this.mesh);
+        } else {
+            if (scene) {
+                scene.add(this.mesh);
+            } else {
+                throw 'Either celestialBody or scene must be provided!';
+            }
+        }
+    };
+
+    /** Creates a mesh for the orbit ring */
+    var setRing = () => {
+        if (celestialBody) {
+            var ringSize = 0.1;
+            var ringGeometry = new THREE.RingGeometry(orbitingDistance - ringSize / 2, orbitingDistance + ringSize / 2, 40);
+            var ringMaterial = new THREE.MeshBasicMaterial({ color: color || 'white', side: THREE.DoubleSide });
+            this.orbitRing = new THREE.Mesh(ringGeometry, ringMaterial);
+            this.orbitRing.matrixAutoUpdate = false;
+            this.orbitRing.matrix = new THREE.Matrix4().makeRotationX(Math.PI / 2);
+            celestialBody.mesh.add(this.orbitRing);
+        }
+    };
+
+    setMesh();
+    setRing();
 
     /** Makes the celestial body orbit
      * @param deltaTime Milliseconds from the last frame
@@ -142,9 +175,10 @@ var CelestialBody = function ({
         this.mesh.matrix = rot.multiply(transl);
     };
 
-    this.setOrbitVisible = function (visible) {
+    /** Toggles orbit ring visibility */
+    this.toggleOrbitVisible = function () {
         if (this.orbitRing) {
-            this.orbitRing.visible = visible;
+            this.orbitRing.visible = !this.orbitRing.visible;
         }
     };
 };
